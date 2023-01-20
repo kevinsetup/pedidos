481617926ec384ebb5dfbe5c7b2d50fb70f1ff51
Jezer Rázuri <jezerrazuri@upeu.edu.pe>
2023-01-20 12:40:29 -0500

    detail

--- src/app/modules/pedidos/gestionpedidos/detallepedido/detallepedido.component.ts
+++ src/app/modules/pedidos/gestionpedidos/detallepedido/detallepedido.component.ts
@@ -2,17 +2,15 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { showExitConfirm } from '../../../../utils/dialog.util';
 
 @Component({
-  selector: 'app-detallepedido',
-  templateUrl: './detallepedido.component.html',
-  styleUrls: ['./detallepedido.component.scss']
+    selector: 'app-detallepedido',
+    templateUrl: './detallepedido.component.html',
+    styleUrls: ['./detallepedido.component.scss'],
 })
 export class DetallepedidoComponent {
-  @Output() eventEmitter = new EventEmitter<Boolean>(); 
+    @Output() eventEmitter = new EventEmitter<Boolean>();
 
-  verdetalle:boolean = false;
-  showModal3() {
-  
-        this.eventEmitter.emit(false)
-   
-  }
+    verdetalle: boolean = false;
+    showModal3() {
+        this.eventEmitter.emit(false);
+    }
 }
--- src/app/modules/pedidos/gestionpedidos/detallepedido/verdetalle/verdetalle.component.ts
+++ src/app/modules/pedidos/gestionpedidos/detallepedido/verdetalle/verdetalle.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-verdetalle',
-  templateUrl: './verdetalle.component.html',
-  styleUrls: ['./verdetalle.component.scss']
+    selector: 'app-verdetalle',
+    templateUrl: './verdetalle.component.html',
+    styleUrls: ['./verdetalle.component.scss'],
 })
 export class VerdetalleComponent {
-
+    @Output() hideDetail = new EventEmitter<Boolean>();
+    detailProducts: any = [
+        {
+            productId: '216714852',
+            productName: 'Chancay',
+            quantity: 3,
+            unitPrice: 0.9,
+        },
+        {
+            productId: '216714852',
+            productName: 'Chancay',
+            quantity: 3,
+            unitPrice: 0.9,
+        },
+        {
+            productId: '216714852',
+            productName: 'Chancay',
+            quantity: 3,
+            unitPrice: 0.9,
+        },
+        {
+            productId: '216714852',
+            productName: 'Chancay',
+            quantity: 3,
+            unitPrice: 0.9,
+        },
+        {
+            productId: '216714852',
+            productName: 'Chancay',
+            quantity: 3,
+            unitPrice: 0.9,
+        },
+        {
+            productId: '216714852',
+            productName: 'Chancay',
+            quantity: 3,
+            unitPrice: 0.9,
+        },
+        {
+            productId: '216714852',
+            productName: 'Chancay',
+            quantity: 3,
+            unitPrice: 0.9,
+        },
+    ];
+    hideOrder() {
+        this.hideDetail.emit(false);
+    }
 }
